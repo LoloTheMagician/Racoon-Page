d5cb5c8f2ea36727f67c128f8012da7b2db29d3f
loloTheMagician <elperiplodelolo@gmail.com>
2024-05-01 23:34:55 +0200

    refactor(src): Código eliminado o agregado

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import Head from "./Head.astro";
 import Nav from "./Nav.astro";
 import Seo from "./Seo.astro";
 import { SmoothScroll } from "./SmoothScroll";
+import { Text } from "./Text";
 
 export {
     Head,
     Nav,
     Seo,
-    SmoothScroll
+    SmoothScroll,
+    Text
 }
