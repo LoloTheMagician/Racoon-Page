aa38c4692ab469db3fe9f692af7c93eec7902d3d
loloTheMagician <elperiplodelolo@gmail.com>
2024-05-01 23:29:53 +0200

    feat(scroll): Scroll Smooth con Lenis

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -6,9 +6,6 @@ function SmoothScroll({ children }) {
         <ReactLenis options={{
             duration: 1,
             lerp: 0.05,
-            easing: (delta) => delta < 0.5
-                ? 4 * delta * delta * delta
-                : 1 - Math.pow(-2 * delta + 2, 3) / 2
         }} root>
             {
                 children
