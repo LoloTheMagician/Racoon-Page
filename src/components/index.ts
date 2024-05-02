d82f25efba6b6f985b3af7a571545ce7e64edfb4
loloTheMagician <elperiplodelolo@gmail.com>
2024-05-02 14:39:52 +0200

    feat(paragraph): Implementación del componente Paragraph

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import Nav from "./Nav.astro";
 import Seo from "./Seo.astro";
 import { SmoothScroll } from "./SmoothScroll";
 import { Text } from "./Text";
+import { Paragraph } from "./Paragraph";
 
 export {
     Head,
     Nav,
     Seo,
     SmoothScroll,
-    Text
+    Text,
+    Paragraph
 }
